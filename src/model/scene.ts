21cbe3c26b4618ff12906ec43318d9b8450e947b
FreddyWordingham <freddy@digilab.co.uk>
2023-07-23 23:00:08 +0100

    Added model.scene class

---
+++ src/model/scene.ts
@@ -0,0 +1,35 @@
+import { vec3, mat4 } from "gl-matrix";
+
+import { Camera } from "./camera";
+import { Transformation } from "./transformation";
+
+export class Scene {
+    transformations: Transformation[];
+    cameras: Camera[];
+
+    constructor() {
+        // Transformations
+        this.transformations = [new Transformation([2, 0, 0], 0)];
+
+        // Cameras
+        this.cameras = [new Camera([-2, 0, 0.5], 0, 0)];
+    }
+
+    update() {
+        this.transformations.forEach((transformation) => {
+            transformation.update();
+        });
+
+        this.cameras.forEach((camera) => {
+            camera.update();
+        });
+    }
+
+    get_player_camera(): Camera {
+        return this.cameras[0];
+    }
+
+    get_transformations(): Transformation[] {
+        return this.transformations;
+    }
+}
