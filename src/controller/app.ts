af551dbd6ba993a0814b95642f825587c7d514f8
FreddyWordingham <freddy@digilab.co.uk>
2023-07-25 02:01:21 +0100

    Improved controller.App input display

--- src/controller/app.ts
+++ src/controller/app.ts
@@ -2,25 +2,46 @@ import $ from "jquery";
 
 import { Renderer } from "../view/renderer";
 import { Scene } from "../model/scene";
-import { HUD } from "./hud";
 
 export class App {
     canvas: HTMLCanvasElement;
     renderer: Renderer;
     scene: Scene;
-    hud: HUD;
+
+    key_label: HTMLElement;
+    mouse_x_label: HTMLElement;
+    mouse_y_label: HTMLElement;
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
         this.renderer = new Renderer(canvas);
         this.scene = new Scene();
-        this.hud = new HUD();
 
-        $(document).on("keypress", this.handle_keypress);
+        this.key_label = <HTMLElement>$("#key_label")[0];
+        $(document).on("keypress", (event) => {
+            this.handle_key_press(event);
+        });
+
+        this.mouse_x_label = <HTMLElement>$("#mouse_x_label")[0];
+        this.mouse_y_label = <HTMLElement>$("#mouse_y_label")[0];
+        $(document).on("mousemove", (event) => {
+            this.handle_mouse_move(event);
+        });
     }
 
     async init() {
         await this.renderer.init();
+    }
+
+    handle_key_press(event: JQuery.KeyPressEvent) {
+        this.key_label.innerHTML = event.code;
+    }
+
+    handle_mouse_move(event: JQuery.MouseMoveEvent) {
+        const rect = this.canvas.getBoundingClientRect();
+
+        this.mouse_x_label.innerHTML = String((event.pageX - rect.left) / this.canvas.width);
+        this.mouse_y_label.innerHTML = String(1.0 - (event.pageY - rect.top) / this.canvas.height);
     }
 
     run = () => {
@@ -34,9 +55,5 @@ export class App {
         if (running) {
             requestAnimationFrame(this.run);
         }
-    };
-
-    handle_keypress = (event: JQuery.KeyPressEvent) => {
-        this.hud.handle_keypress(event);
     };
 }
